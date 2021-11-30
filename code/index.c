c93087c2cba722598f9c03e2988227631bad7c7f
Detzz <a2z.team.198@gmail.com>
2021-11-30 14:31:29 +0700

    add c example

---
+++ code/index.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char reply;
+    reply = 'y';
+    printf("your reply = %c", reply);
+    return 0;
+}
---
+++ code/mention.md
@@ -0,0 +1,3 @@
+# compile
+
+gcc "file.c" -o "out" || ./out
