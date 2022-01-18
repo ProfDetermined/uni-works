9831738d96829d576f2253a02a6e5665178d399e
dethMastery <a2z.team.198@gmail.com>
2022-01-18 21:49:17 +0700

    resome code

--- code/index.c
+++ code/index.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+int Feet2Inch(int);
+int feet, inches;
+
 int main() {
-    char reply;
-    reply = 'y';
-    printf("your reply = %c", reply);
-    return 0;
+    feet = 6;
+    inches = Feet2Inch (feet);
+    printf ("Height in Inches is %d", inches);
 }
+
+int Feet2
