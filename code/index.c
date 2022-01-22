01a9384d5c760d7ea2a2e021fb08227a126c8ac7
dethMastery <owner@detzz.in.th>
2022-01-22 11:18:06 +0700

    today class

--- code/index.c
+++ code/index.c
@@ -2,31 +2,7 @@
 #include <curses.h>
 
 int main() {
-    int age[20];
-    int count = 0;
-
-    // do
-    // {
-    //     age[count] = count;
-    //     printf("Now Age = %d\n", count);
-    //     count++;
-    // } while (count < 20);
-
-    // printf("Now Age = %d\n", count);
-    // printf("End\n");
-
-    for (count = 0; count < 20; count++) {
-        int sCount = count + 1;
-        printf("Enter Age [User %d] : ", sCount);
-        scanf("%d", &age[count]);
-    }
-
-    printf("\n\n");
-
-    for (count = 0; count < 20; count++) {
-        int sCount = count + 1;
-        printf("User %d's Age = %d\n", sCount,  age[count]);
-    }
+    
 
     return 0;
 }
