a82e23bfdc9568b32a01b882c96fb1b415a6a5c0
dethMastery <owner@detzz.in.th>
2022-01-22 11:17:49 +0700

    rm unused code

--- code/22-1-22/06-age-inp.c
+++ code/22-1-22/06-age-inp.c
@@ -5,16 +5,6 @@ int main() {
     int age[20];
     int count = 0;
 
-    // do
-    // {
-    //     age[count] = count;
-    //     printf("Now Age = %d\n", count);
-    //     count++;
-    // } while (count < 20);
-
-    // printf("Now Age = %d\n", count);
-    // printf("End\n");
-
     for (count = 0; count < 20; count++) {
         int sCount = count + 1;
         printf("Enter Age [User %d] : ", sCount);
