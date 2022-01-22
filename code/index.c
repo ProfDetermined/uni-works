90d8d8f1ebbcbf471c3dc242cbec347bf3b371d4
dethMastery <owner@detzz.in.th>
2022-01-22 11:00:26 +0700

    today class

--- code/index.c
+++ code/index.c
@@ -2,13 +2,7 @@
 #include <curses.h>
 
 int main() {
-    int year[5] = {2552, 2553, 2554, 2555, 2556};
-    int i;
-
-    for (i = 0; i < 5; i++) {
-        int iOut = i + 1;
-        printf("%d. %d\n", iOut,  year[i]);
-    }
+    
 
     return 0;
 }
