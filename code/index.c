5ad62b26ad8da5feeb3ab6f7f1a94f7e6e5535b7
dethMastery <owner@detzz.in.th>
2022-01-25 14:57:54 +0700

    today class

--- code/index.c
+++ code/index.c
@@ -2,13 +2,7 @@
 // #include <curses.h>
 
 int main() {
-    int z = 10;
-    int *ZPtr;
-    ZPtr = &z;
-
-    printf("Z = %d\n", z);
-    printf("Z pointer = %p\n", *ZPtr);
-    printf("Z pointer = %p\n", ZPtr);
+    
 
     return 0;
 }
