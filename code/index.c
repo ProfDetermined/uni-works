cc7556e8efb79854d01cd368e290bf8e3be29b49
dethMastery <owner@detzz.in.th>
2022-02-01 14:03:00 +0700

    today class

--- code/index.c
+++ code/index.c
@@ -2,7 +2,19 @@
 // #include <curses.h>
 
 int main() {
-    
-   
+    int x[5] = {12,3,34,56,70};
+    int *ip = &x[0];
+    int y = *ip;
+
+    printf("x's memory : %p\n", &x);
+    printf("ip's memory : %p\n", &ip); 
+    printf("ip's value : %p\n", &x);
+
+    printf("\n\n");
+
+    printf("ip's value : %p\n", ip);
+    printf("pointer's value that point to ip : %d\n", *ip);
+    printf("y's value : %d\n", y);
+
     return 0;
 }
