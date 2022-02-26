a2812c08d735b071c241954f5cf5fc2b881565e3
Suphakit P <a2z.team.198@gmail.com>
2022-02-26 14:25:38 +0700

    updated: clearing board

--- code/index.c
+++ code/index.c
@@ -8,24 +8,11 @@
 // #include <curses.h>
 
 
-int func(int x) {
-    printf("\n");
-    printf("Inside cbv x = %d before +10. \n", x);
-    x += 10;
-    printf("Inside cbv x = %d after +10. \n", x);
-    printf("\n");
+int func() {
+
 }
 
 void main() {
-    int a = 10;
-
-    printf("\n");
-    
-    printf("a = %d before cbv funtion. \n", a);
-    func(a);
-    printf("a = %d after cbv function. \n", a);
-    
-    printf("\n");
 
     // return 0;
 }
