a1a384fd1225364e4ca3c74c1503f8699d6bb227
Suphakit P <a2z.team.198@gmail.com>
2022-02-26 12:42:29 +0700

    updated: clearing board

--- code/index.c
+++ code/index.c
@@ -7,22 +7,12 @@
 
 // #include <curses.h>
 
-int num;
+int func() {
 
-int func(int n) {
-    int i = 1;
-    for(i = 1; i <= n; i++){
-        printf("*");
-    }
-    printf("\n");
 }
 
 void main() {
-    num = 9;
-    func(num);
-    printf("*CPE-UP*");
-    printf("\n");
-    func(num);
+
 
     // return 0;
 }
