22e1fdddf835f031ca8f1c4ee4c0f63e84c6dae7
Suphakit P <a2z.team.198@gmail.com>
2022-02-26 13:07:37 +0700

    updated: clear board

--- code/index.c
+++ code/index.c
@@ -8,23 +8,12 @@
 // #include <curses.h>
 
 
-int func(int *a, int *b) {
-    int temp;
-    printf("swap: a = %d, b = %d\n", *a, *b);
-
-    temp = *a;
-    *a = *b;
-    *b = temp;
-
-    printf("swap: a = %d, b = %d\n", *a, *b);
+int func() {
+    
 }
 
 void main() {
-    int x = 5, y = 10; 
-
-    printf("main: x = %d, y = %d\n", x, y);
-    func(&x, &y);
-    printf("main: x = %d, y = %d\n", x, y);
+    
     
     // return 0;
 }
