588046b185e0e6e4089f72b97147292f07ce3a83
Suphakit P <a2z.team.198@gmail.com>
2022-02-26 13:00:05 +0700

    updated: clearing board

--- code/index.c
+++ code/index.c
@@ -9,19 +9,11 @@
 
 
 int func(int a, int b) {
-    int temp;
-    printf("swap: a = %d, b = %d\n", a, b);
-    temp = a;
-    a = b;
-    b = temp;
-    printf("swap: a = %d, b = %d\n", a, b);
+
 }
 
 void main() {
-    int x = 5, y = 10;
-    printf("main: x = %d, y = %d\n", x, y);
-    func(x,y);
-    printf("main: x = %d, y = %d\n", x, y);
-
+    
+    
     // return 0;
 }
