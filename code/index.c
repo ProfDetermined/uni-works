183b29b96e64050e703e8475bb16cae501b2ec47
Suphakit P <a2z.team.198@gmail.com>
2022-02-26 14:15:33 +0700

    updated: clearing board

--- code/index.c
+++ code/index.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 // #include <ctype.h>
-#include <conio.h>
+// #include <conio.h>
 // #include <math.h>
 // #include <stdlib.h>
-#include <string.h>
+// #include <string.h>
 
 // #include <curses.h>
 
@@ -13,15 +13,7 @@
 // }
 
 void main() {
-    char str1[30] = "Current Date ";
-    char str2[30] = "18 August 2008";
-    char str3[30];
     
-    strcpy(str3, str1);
-    strcpy(str1, str2);
-
-    printf("str1 = %s\n", str1);
-    printf("str3 = %s\n", str3);
     
     // return 0;
 }
