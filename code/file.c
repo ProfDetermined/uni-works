a0cbe8d2e729a5c0418be526cf78a5dafef6aaaf
dethMastery <owner@detzz.in.th>
2022-06-13 18:11:41 +0700

    updated: clearing board

--- code/file.c
+++ code/file.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a = 9, b = 4, c;
-
-    c=a+b;
-    printf("a = %d, b = %d, [a+b]=c, c = %d \n", a, b, c);
-
-    c=a-b;
-    printf("a = %d, b = %d, [a-b]=c, c = %d \n", a, b, c);
-
-    c=a*b;
-    printf("a = %d, b = %d, [a*b]=c, c = %d \n", a, b, c);
-
-    c=a/b;
-    printf("a = %d, b = %d, [a/b]=c, c = %d \n", a, b, c);
-
-    c=a%b;
-    printf("a = %d, b = %d, [a%%b]=c, c = %d \n", a, b, c);
-
+    
 
     return 0;
 }
