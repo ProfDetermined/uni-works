11842f70912e9bb7ab60b3137ee38c4ae6f05ece
dethMastery <owner@detzz.in.th>
2022-07-17 11:17:30 +0700

    updated: rename ;D

--- code/file.c
+++ code/file.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  
+  char input[5];
+
+  printf("input : ");
+  scanf("%s", input);
+
+  printf("%c\n", input[0]);
+  printf("%c\n", input[1]);
+  printf("%c\n", input[2]);
+  printf("%c\n", input[3]);
+  printf("%c\n", input[4]);
 
   return 0;
 }
