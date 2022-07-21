84f1f99330f7633dbc9b5fdf766d9b9e88953a3a
dethMastery <owner@detzz.in.th>
2022-07-21 15:22:49 +0700

    init: setting up new branch

--- code/file.c
+++ code/file.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int a=4, b=5;
-
-
-  (a>b) ? printf("hey a is greater than b\n") : (a<b) ? printf("hey b is greater than a\n") : printf("ah.. a is equal b");
+  
   
   return 0;
 }
