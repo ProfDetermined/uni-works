46c65f6e51900c6f24b245adfe61c705ac093948
dethMastery <owner@detzz.in.th>
2022-08-01 18:34:10 +0700

    updated: code

--- code/file.c
+++ code/file.c
@@ -6,18 +6,29 @@ int main() {
 
   int *ptr;
   printf("ptr memory add. = %p\n", ptr);
+  printf("y memory add. = %p\n", &y);
+ 
+  printf("\n");
 
   ptr = &count;
   printf("ptr memory add. = %p\n", ptr);
 
+  printf("\n");
+
   y = *ptr;
   printf("y memory add. = %p\n", &y);
+  printf("y value = %d\n", y);
+
+  printf("\n");
 
   *ptr = 0;
   printf("ptr memory add. = %p\n", ptr);
   
+  printf("\n");
+
   ptr = &z[0];
   printf("ptr memory add. = %p\n", ptr);
+  printf("z memory add. = %p\n", &z);
 
   return 0;
 }
