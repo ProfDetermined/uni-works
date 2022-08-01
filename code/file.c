fdf336c4570de2c7220182e4cd95a8570c1828d2
dethMastery <owner@detzz.in.th>
2022-08-01 17:37:36 +0700

    added: array-matrix

--- code/file.c
+++ code/file.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <ncurses.h> // using instead of conio.h
 
+#define width 5
+#define height 3
+
 int main() {
-  int age[20], i;
-
-  for(i=0; i<3; i++) {
-    printf("Enter your age : ");
-    scanf("%d", &age[i]);
-  }
-
-  printf("\n------------------------------------------------------------------------------------------------------------------\n\n");
-
-  for(i=0; i<3; i++) {
-    printf("Age #%d :: %d\n", i+1, age[i]);
-  }
   
+
   return 0;
 }
