cb736168ef2d890de1e1cec117f34bfa820f8e96
dethMastery <me@suphakit.net>
2022-11-24 10:21:40 +0700

    added: blank template

--- code/main.java
+++ code/main.java
@@ -1,7 +1,5 @@
 public class main {
-	public static void main(String[] args) {
-		System.out.println("Hello, World! Java");
-		System.out.println("Suphakit Pinyoworapot");
-		System.out.println("64023814");
+	public static void func(String[] args) {
+		
 	}
 }
